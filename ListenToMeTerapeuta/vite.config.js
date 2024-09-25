efe3129997d3a7991c0d7fba52affb2b204e09d4
deivid0505 <1120209@unav.edu.mx>
2024-09-24 17:33:30 -0700

    viteconfig

--- ListenToMeTerapeuta/vite.config.js
+++ ListenToMeTerapeuta/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/tu-repo/', // Cambia esto según tu caso
+  base: '/', // Cambia esto según tu caso
 });
